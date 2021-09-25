3b3cdcab2210f9e48bf5224ebf486c65b7dc95f4
Sumit Vairagar <sumitvairagar@Sumits-MacBook-Pro.local>
2021-09-25 15:47:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Exchanges, Homepage, CryptoCurrencies, News, CryptoCurrencyDetails } from './components';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar></Navbar>
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/">
+                                <Homepage/>
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges/>
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <CryptoCurrencies/>
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoCurrencyDetails/>
+                            </Route>
+                            <Route exact path="/news">
+                                <News></News>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/CryptoCurrencyDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoCurrencyDetails = () => {
+    return (
+        <div>
+            CryptoCurrencyDetails
+        </div>
+    )
+}
+
+export default CryptoCurrencyDetails
